1598dedd1387bd84024981bea38fca78ca17c9b1
chrstphrbtchr <chrstphrbtchr@users.noreply.github.com>
2019-08-28 17:14:37 -0400

    Let the universe decide!

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 
 # 1. Updated LICENSE
 # 2. Completed answers
-# 3. Debated what makes a "favorite game"
 # 4. Change my answer seven times...
 # 5. Updated README.md (This)
 # 6. Changed my mind again, but decided to leave it alone
--- main.py
+++ main.py
@@ -6,8 +6,12 @@ utils.check_version((3,7))
 utils.clear()
 
 print('Hello, my name is Christopher Butcher!')
-print('My favorite game is Earthbound, but this is a difficult question so that will change.')
+import random
+gamez = ['Earthbound','Super Mario World','Fallout 4','Borderlands 2','Horizon Zero Dawn']
+favgame = random.choice(gamez)
+print('My favorite game is', favgame,', but this is a difficult question so that will change.')
 print('I am slightly concerned about keeping up with learning these new languages and concepts.')
 print('However, I am excited to learn and to be surrounded by those who can help me!')
+print('I enjoy performing and writing music, mostly for myself and my dog.')
 print('My stackoverflow.com user number is 11981611')
 print('Find me on GITHUB at https://github.com/chrstphrbtchr (That is my name without vowels if that helps) ')
